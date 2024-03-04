857bb2659eebbcb6dde55b562fdab43c05f66649
SOURCE-LORD <161044920+SOURCE-LORD@users.noreply.github.com>
2024-03-04 17:36:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SOURCE-LORD/QURAN-PRO.git",
+    "https://github.com/SOURCE-LORD/TEST.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
